6dd1f9fef9469b2f55a996e6865ccdf2805081ec
Wiryono Lauw <wiryonolau@gmail.com>
2020-11-21 13:32:30 +0700

    Fix Config

--- src/Config.php
+++ src/Config.php
@@ -2,22 +2,26 @@
 
 namespace App;
 
-class Config {
+class Config
+{
     protected $config = [];
 
-    public function __construct($config_dir) {
-        $this->parseConfig($config_dir);
+    public function __construct($config_dirs)
+    {
+        $this->parseConfig($config_dirs);
     }
 
-    public function getConfig() {
+    public function getConfig()
+    {
         return $this->config;
     }
 
-    private function parseConfig($config_dir) {
-        $config_path = realpath($config_dir);                                                                                                             
-        foreach (glob(sprintf("%s/*.config.php", $config_dir)) as $file) {
-            $this->config = array_merge_recursive($this->config, include $file);
+    private function parseConfig($config_dirs)
+    {
+        foreach ($config_dirs as $config_dir) {
+            foreach (glob(sprintf("%s/*.config.php", $config_dir)) as $file) {
+                $this->config = array_merge_recursive($this->config, include $file);
+            }
         }
     }
 }
-?>
