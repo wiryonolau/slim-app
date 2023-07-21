17825e77304dadb549c43e334ed5064237d3c305
Wiryono Lauw <wiryonolau@gmail.com>
2023-07-21 16:35:37 +0700

    HttpException check for ajax or json output request

--- src/Http/HttpExceptionMiddleware.php
+++ src/Http/HttpExceptionMiddleware.php
@@ -32,6 +32,27 @@ class HttpExceptionMiddleware extends AbstractMiddleware
 
             $response = new Response();
 
+            // Check if this is a ajax request then prevent from sending html error
+            if ($this->asJson($request)) {
+                $payload = [
+                    'jsonrpc' => '2.0',
+                    'id' => time(),
+                    'error' => [
+                        'code' => -32603,
+                        'message' => sprintf(
+                            "%s %s",
+                            $httpException->getCode(),
+                            $httpException->getMessage()
+                        )
+                    ],
+                ];
+
+                $response->getBody()->write(json_encode($payload));
+                return $response
+                    ->withHeader('Content-Type', 'application/json')
+                    ->withStatus(200);
+            }
+
             $code = $httpException->getCode();
             $template = sprintf('/error/%d', $code);
 
@@ -40,6 +61,26 @@ class HttpExceptionMiddleware extends AbstractMiddleware
             ];
 
             return $this->view->render($response, $template, $variables);
+        }
+    }
+
+    protected function asJson(Request $request): bool
+    {
+        try {
+            if ($request->getQueryParams()["format"] == "json") {
+                return true;
+            }
+
+            if ($request->getQueryParams()["output"] == "json") {
+                return true;
+            }
+
+            if ($request->getHeaderLine("X-Requested-With") == "XMLHttpRequest") {
+                return true;
+            }
+            return false;
+        } catch (Exception $e) {
+            return false;
         }
     }
 }
