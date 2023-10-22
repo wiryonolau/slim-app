f8c43aa8aefa6a5c7d32acd9a07817534024787c
Wiryono Lauw <wiryonolau@gmail.com>
2023-10-22 08:48:24 +0700

    add http json action, search http query and post using path

--- src/Action/InvokableAction.php
+++ src/Action/InvokableAction.php
@@ -8,6 +8,7 @@ use Exception;
 use Fig\Http\Message\RequestMethodInterface;
 use Fig\Http\Message\StatusCodeInterface;
 use Itseasy\Http\HttpRequest;
+use Itseasy\Stdlib\ArrayUtils;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Exception\HttpException;
@@ -20,6 +21,12 @@ class InvokableAction extends AbstractAction
     protected $response = null;
     protected $parsedBody = [];
     protected $arguments = [];
+
+    // Allow method fallback from json to normal method if not exist
+    // e.g httpGetJson to httpGet
+    protected $jsonFallback = true;
+
+    private $isJsonRequest = false;
 
     public function __invoke(
         ServerRequestInterface $request,
@@ -49,13 +56,12 @@ class InvokableAction extends AbstractAction
 
         $this->parseRequest();
 
-        $method = strtolower($method);
-        $function_name = sprintf("%s%s", "http", ucfirst($method));
+        $this->isJsonRequest = HttpRequest::asJson($this->request);
 
-        // call function base on http method e.g httpGet, httpPost, etc
-        if (!method_exists($this, $function_name)) {
-            $this->forbidden("Action not exist");
-        }
+        $method = strtolower($method);
+        $action_method = ucfirst($method);
+        $http_function_name = sprintf("%s%s", "http", $action_method);
+        $json_function_name = sprintf("%s%s%s", "http", $action_method, "Json");
 
         $this->getEventManager()->trigger(
             sprintf('action.%s.pre', $method),
@@ -67,7 +73,31 @@ class InvokableAction extends AbstractAction
             ]
         );
 
-        $actionResponse = call_user_func_array([$this, $function_name], []);
+        // call function that ask for json response , method name is httpGetJson, httpPostJson, etc
+        // function must return array or implement getArrayCopy
+        // response will be return as json directly
+        // if not exists fallback to normat httpGet, httpPost funciton
+        if ($this->asJson() and method_exists($this, $json_function_name)) {
+            $actionResponse = call_user_func_array([$this, $http_function_name], []);
+            if (is_array($actionResponse)) {
+                return $this->renderAsJson($actionResponse);
+            }
+
+            if (method_exists($actionResponse, "getArrayCopy")) {
+                return $this->renderAsJson($actionResponse->getArrayCopy());
+            }
+        }
+
+        if ($this->asJson() and !$this->jsonFallback) {
+            $this->forbidden("Action not exist");
+        }
+
+        // call function base on http method e.g httpGet, httpPost, etc
+        if (!method_exists($this, $http_function_name)) {
+            $this->forbidden("Action not exist");
+        }
+
+        $actionResponse = call_user_func_array([$this, $http_function_name], []);
 
         $this->getEventManager()->trigger(
             sprintf('action.%s.post', $method),
@@ -105,9 +135,14 @@ class InvokableAction extends AbstractAction
      */
     public function renderAsJson(
         array $variables = [],
-        ?int $id = null
+        ?int $id = null,
+        ?int $http_status_code = null
     ): ResponseInterface {
-        return HttpRequest::jsonRpcResponse($variables, $id);
+        return HttpRequest::jsonRpcResponse(
+            $variables,
+            $id,
+            $http_status_code
+        );
     }
 
 
@@ -125,7 +160,12 @@ class InvokableAction extends AbstractAction
         $ignore_empty = false
     ) {
         try {
-            $value = $this->request->getQueryParams()[$key] ?? null;
+            $value = ArrayUtils::query(
+                $this->request->getQueryParams(),
+                $key,
+                null
+            );
+
             if ($ignore_empty) {
                 return $value;
             }
@@ -162,7 +202,12 @@ class InvokableAction extends AbstractAction
             if (!in_array($this->request->getMethod(), ["POST", "PUT"])) {
                 throw new Exception("Request is not a POST or PUT");
             }
-            $value = $this->parsedBody[$key] ?? null;
+
+            $value = ArrayUtils::query(
+                $this->parsedBody,
+                $key,
+                null
+            );
 
             if ($ignore_empty) {
                 return $value;
@@ -222,6 +267,6 @@ class InvokableAction extends AbstractAction
 
     protected function asJson(): bool
     {
-        return HttpRequest::asJson($this->request);
+        return $this->isJsonRequest;
     }
 }
--- src/Http/HttpRequest.php
+++ src/Http/HttpRequest.php
@@ -21,11 +21,13 @@ class HttpRequest
                 return true;
             }
 
-            if ($request->getQueryParams()["format"] == "json") {
+            $queries = $request->getQueryParams();
+
+            if (isset($queries["format"]) and $queries["format"] == "json") {
                 return true;
             }
 
-            if ($request->getQueryParams()["output"] == "json") {
+            if (isset($queries["output"]) and $queries["output"] == "json") {
                 return true;
             }
 
@@ -47,20 +49,20 @@ class HttpRequest
             if (!empty($variables["error"])) {
                 $payload =  [
                     'jsonrpc' => '2.0',
-                    'id' => (is_null($id) ? time() : $id),
+                    'id' => $id ?: time(),
                     'error' => $variables["error"]
                 ];
             } else {
                 $payload = [
                     'jsonrpc' => '2.0',
-                    'id' => (is_null($id) ? time() : $id),
+                    'id' => $id ?: time(),
                     'result' => (empty($variables["result"]) ? $variables : $variables["result"]),
                 ];
             }
         } catch (Exception $e) {
             $payload = json_encode([
                 'jsonrpc' => '2.0',
-                'id' => (is_null($id) ? time() : $id),
+                'id' => $id ?: time(),
                 'error' => [
                     'code' => -32603,
                     'message' => $e->getMessage(),
